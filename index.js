a4c7f67894b0d6ec321201504c4df60a68a96794
Vavavoom <42918656+Thaleia@users.noreply.github.com>
2023-11-18 15:17:40 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from './pages/App';
+import ReactDOM from "react-dom";
+import 'font-awesome/css/font-awesome.min.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
