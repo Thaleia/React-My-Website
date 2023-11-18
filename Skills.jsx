f00f3743ead9d4358f64a646d070954c5e938463
Vavavoom <42918656+Thaleia@users.noreply.github.com>
2023-11-18 22:56:33 +0000

    softskills setup in Skills.jsx

--- Skills.jsx
+++ Skills.jsx
@@ -1,19 +1,38 @@
 
 import React from 'react';
 import { hardSkills } from '../data/data.js';
+import { softSkills } from '../data/data.js';
 import "./skills.css";
 
 const Skills = () => {
   return (
     <div className='skills'>
-      <ul>
-        {hardSkills.map((hardSkill) => (
-          <li key={hardSkill.id}>
-            <div>{hardSkill.name}</div>
-            <img key={hardSkill.id} src={hardSkill.image} alt={hardSkill.name} />
-          </li>
-        ))}
-      </ul>
+      <h1>Hard skills</h1>
+      <div>
+        <ul>
+          {hardSkills.map((hardSkill) => (
+            <li key={hardSkill.id}>
+              <div>
+                <p>{hardSkill.name}</p>
+               <span>{hardSkill.image}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <h1>Soft skills</h1>
+      <div>
+        <ul>
+          {softSkills.map((softSkill) => (
+            <li key={softSkill.id}>
+              <div>
+                <p>{softSkill.name}</p>
+               <span>{softSkill.image}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
