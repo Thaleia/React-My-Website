d98c53e597bf667ab01cb917aa2b929c7ca77104
Vavavoom <42918656+Thaleia@users.noreply.github.com>
2023-11-30 12:08:11 +0000

    About js and css re-upload

--- App.js
+++ App.js
@@ -1,19 +1,42 @@
-import './App.css';
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Layout from "./pages/Layout";
-import Home from "./pages/Home";
-import Contact from "./pages/Contact";
-import Projects from "./pages/Projects";
-import Skills from "./pages/Skills";
-import About from "./pages/About";
-
-
-
-
-import NoPage from "./pages/NoPage";
+import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Home';
+import Contact from './Contact';
+import Projects from './Projects';
+import Skills from './Skills';
+import About from './About';
+import NoPage from './NoPage';
 
 function App() {
+  const [hardSkills, setHardSkills] = useState([]);
+  const [softSkills, setSoftSkills] = useState([]);
+  const [cart, setCart] = useState([]);
+  const [allSkillsAdded, setAllSkillsAdded] = useState(false);
+
+  const addToHardSkills = (item) => {
+    setHardSkills([...hardSkills, item]);
+    checkAllSkillsAdded();
+  };
+
+  const addToSoftSkills = (item) => {
+    setSoftSkills([...softSkills, item]);
+    checkAllSkillsAdded();
+  };
+
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+    checkAllSkillsAdded();
+  };
+
+  const checkAllSkillsAdded = () => {
+    const totalSkills = 30;
+
+    if (hardSkills.length + softSkills.length + cart.length === totalSkills) {
+      setAllSkillsAdded(true);
+    }
+  };
+
   return (
     <BrowserRouter>
       <Routes>
@@ -21,12 +44,13 @@ function App() {
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
           <Route path="projects" element={<Projects />} />
-          <Route path="skills" element={<Skills />} />
+          <Route path="skills" element={<Skills allSkillsAdded={allSkillsAdded} cart={cart} addToCart={addToCart} addToHardSkills={addToHardSkills} addToSoftSkills={addToSoftSkills} />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="*" element={<NoPage />} /> 
+          <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
 }
+
 export default App;
